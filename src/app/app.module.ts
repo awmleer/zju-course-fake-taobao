a9790cad89b9277a9dfe021a7de2de6aaf139ece
awmleer <awmleer@sparker.xyz>
2018-04-01 10:19:56 +0800

    main search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MainSearchComponent } from './main-search/main-search.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainSearchComponent
   ],
   imports: [
     BrowserModule
