500b7f4e30d2053c9ffc779dd77a3cc12d4dcefc
awmleer <awmleer@sparker.xyz>
2018-04-27 09:34:49 +0800

    componentized

--- src/app/main-search/main-search.component.ts
+++ src/app/main-search/main-search.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainSearchComponent implements OnInit {
 
+  suggestionWords = ['新款连衣裙','四件套','潮流T恤','时尚女鞋','短裤','半身裙','男士外套','墙纸','行车记录仪','新款男鞋','耳机'];
+
   constructor() { }
 
   ngOnInit() {
