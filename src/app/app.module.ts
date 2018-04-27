44628edd5e8b44190701ec97472c635b2f6f3b5f
awmleer <awmleer@sparker.xyz>
2018-04-27 10:34:00 +0800

    init content component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainSearchComponent } from './main-search/main-search.component';
+import { ContentComponent } from './content/content.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainSearchComponent
+    MainSearchComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
